937e6a80fdb03509fc489fa225954fe959b94996
LeoDomGit <leodomsolar@gmail.com>
2023-10-22 07:43:41 +0700

    C

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<GoogleOAuthProvider clientId="161063259833-dumnj31qld4lmbfph52ugelj7hml4a66.apps.googleusercontent.com">
+<GoogleOAuthProvider clientId="161063259833-smgqmto3hh82f08fvjcqs3b1volfinkf.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>
 );
